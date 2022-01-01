d3a3134bf81ad35c992e2194ab26a424bfb43dfe
Jeferson Sabino dos Santos Junior <86162489+JefersonTaiyou@users.noreply.github.com>
2022-01-01 13:23:15 -0300

    Update Structure

--- js/MyPortifolio.js
+++ js/MyPortifolio.js
@@ -1,3 +1,22 @@
+const skillsContent = document.getElementsByClassName('skills-content'),
+    skillsHeader = document.querySelectorAll('.skills-cabecalho');
+
+function toggleSkills() {
+    let itemClass = this.parentNode.className
+
+    for (i = 0; i < skillsContent.length; i++) {
+        skillsContent[i].className = 'skills-content skills-close'
+    }
+
+    if (itemClass === 'skills-content skills-close') {
+        this.parentNode.className = 'skills-content skills-open'
+    }
+}
+
+skillsHeader.forEach((el) => {
+    el.addEventListener('click', toggleSkills)
+})
+
 const navMenu = document.getElementById('nav-menu'),
     navToggle = document.getElementById('nav-toggle'),
     navClose = document.getElementById('nav-close');
